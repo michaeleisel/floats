456f37cefeca9d076aa12c5a79e27e4cb8f92db2
Michael Eisel <meisel@spotify.com>
2021-04-28 13:11:46 -0400

    Initial Commit

---
+++ floats/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  floats
+//
+//  Created by Michael Eisel on 4/28/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
